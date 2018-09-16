b660cea25fb953534e24fa389f283590fa0fee3b
usimonline <usim.online@mail.ru>
2018-09-16 11:10:59 +0300

    1

---
+++ lower_js.php
@@ -0,0 +1,3 @@
+<script src="./vendor/jquery-2.0.3.min.js"></script>
+<script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="./js/ajax-form.js"></script>
---
+++ start.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!empty($_COOKIE['sid'])) {
+// check session id in cookies
+session_id($_COOKIE['sid']);
+}
+
+session_start();
+require_once 'classes/Auth.class.php';
