0e3cfc4f34d99a2701b28cd477e545762ffbb410
usimonline <usim.online@mail.ru>
2018-09-16 07:53:01 +0300

    1

---
+++ ajax.php
@@ -0,0 +1,142 @@
+<?php
+
+include './classes/Auth.class.php';
+include './classes/AjaxRequest.class.php';
+
+if (!empty($_COOKIE['sid'])) {
+    // check session id in cookies
+    session_id($_COOKIE['sid']);
+}
+session_start();
+
+class AuthorizationAjaxRequest extends AjaxRequest
+{
+    public $actions = array(
+        "login" => "login",
+        "logout" => "logout",
+        "register" => "register",
+    );
+
+    public function login()
+    {
+        if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+            // Method Not Allowed
+            http_response_code(405);
+            header("Allow: POST");
+            $this->setFieldError("main", "Method Not Allowed");
+            return;
+        }
+        setcookie("sid", "");
+
+        $login = $this->getRequestParam("login");
+        $password = $this->getRequestParam("password");
+        $remember = !!$this->getRequestParam("remember-me");
+
+        if (empty($login)) {
+            $this->setFieldError("login", "Enter the login");
+            return;
+        }
+
+        if (empty($password)) {
+            $this->setFieldError("password", "Enter the password");
+            return;
+        }
+
+        $user = new Auth\User();
+        $auth_result = $user->authorize($login, $password, $remember);
+
+        if (!$auth_result) {
+            $this->setFieldError("password", "Invalid login or password");
+            return;
+        }
+
+        $this->status = "ok";
+        $this->setResponse("redirect", ".");
+        $this->message = sprintf("Hello ", $login);
+    }
+
+    public function logout()
+    {
+        if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+            // Method Not Allowed
+            http_response_code(405);
+            header("Allow: POST");
+            $this->setFieldError("main", "Method Not Allowed");
+            return;
+        }
+
+        setcookie("sid", "");
+
+        $user = new Auth\User();
+        $user->logout();
+
+        $this->setResponse("redirect", ".");
+        $this->status = "ok";
+    }
+
+    public function register()
+    {
+        if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+            // Method Not Allowed
+            http_response_code(405);
+            header("Allow: POST");
+            $this->setFieldError("main", "Method Not Allowed");
+            return;
+        }
+
+        setcookie("sid", "");
+
+        $login = $this->getRequestParam("login");
+        $password1 = $this->getRequestParam("password1");
+        $password2 = $this->getRequestParam("password2");
+        $email = $this->getRequestParam("email");
+        $name = $this->getRequestParam("name");
+
+        if (empty($login)) {
+            $this->setFieldError("login", "Enter the login");
+            return;
+        }
+
+        if (empty($password1)) {
+            $this->setFieldError("password1", "Enter the password");
+            return;
+        }
+
+        if (empty($email)) {
+            $this->setFieldError("email", "Enter the email");
+            return;
+        }
+
+        if (empty($name)) {
+            $this->setFieldError("name", "Enter the name");
+            return;
+        }
+
+        if (empty($password2)) {
+            $this->setFieldError("password2", "Confirm the password");
+            return;
+        }
+
+        if ($password1 !== $password2) {
+            $this->setFieldError("password2", "Confirm password is not match");
+            return;
+        }
+
+        $user = new Auth\User();
+
+        try {
+            $new_user_id = $user->create($login, $password1);
+        } catch (\Exception $e) {
+            $this->setFieldError("login", $e->getMessage());
+            return;
+        }
+        $user->authorize($login, $password1);
+
+        $this->message = sprintf("Hello, %s! Thank you for registration.", $login);
+        $this->setResponse("redirect", "/");
+        $this->status = "ok";
+    }
+}
+
+$ajaxRequest = new AuthorizationAjaxRequest($_REQUEST);
+$ajaxRequest->showResponse();
---
+++ classes/Auth.class.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Auth;
+
+class User
+{
+    private $id;
+    private $login;
+    private $db;
+    private $user_id;
+
+    private $db_host = "localhost";
+    private $db_name = "u689193950_base";
+    private $db_user = "u689193950_user";
+    private $db_pass = "111111";
+
+    private $is_authorized = false;
+
+    public function __construct($login = null, $password = null)
+    {
+        $this->login = $login;
+        $this->connectDb($this->db_name, $this->db_user, $this->db_pass, $this->db_host);
+    }
+
+    public function __destruct()
+    {
+        $this->db = null;
+    }
+
+    public static function isAuthorized()
+    {
+        if (!empty($_SESSION["user_id"])) {
+            return (bool) $_SESSION["user_id"];
+        }
+        return false;
+    }
+
+    public function passwordHash($password, $salt = null, $iterations = 10)
+    {
+        $salt || $salt = uniqid();
+        $hash = md5(md5($password . md5(sha1($salt))));
+
+        for ($i = 0; $i < $iterations; ++$i) {
+            $hash = md5(md5(sha1($hash)));
+        }
+
+        return array('hash' => $hash, 'salt' => $salt);
+    }
+
+    public function getSalt($login) {
+        $query = "select salt from users where login = :login limit 1";
+        $sth = $this->db->prepare($query);
+        $sth->execute(
+            array(
+                ":login" => $login
+            )
+        );
+        $row = $sth->fetch();
+        if (!$row) {
+            return false;
+        }
+        return $row["salt"];
+    }
+
+    public function authorize($login, $password, $remember=false)
+    {
+        $query = "select id, login from users where
+            login = :login and password = :password limit 1";
+        $sth = $this->db->prepare($query);
+        $salt = $this->getSalt($login);
+
+        if (!$salt) {
+            return false;
+        }
+
+        $hashes = $this->passwordHash($password, $salt);
+        $sth->execute(
+            array(
+                ":login" => $login,
+                ":password" => $hashes['hash'],
+            )
+        );
+        $this->user = $sth->fetch();
+        
+        if (!$this->user) {
+            $this->is_authorized = false;
+        } else {
+            $this->is_authorized = true;
+            $this->user_id = $this->user['id'];
+            $this->saveSession($remember);
+        }
+
+        return $this->is_authorized;
+    }
+
+    public function logout()
+    {
+        if (!empty($_SESSION["user_id"])) {
+            unset($_SESSION["user_id"]);
+        }
+    }
+
+    public function saveSession($remember = false, $http_only = true, $days = 7)
+    {
+        $_SESSION["user_id"] = $this->user_id;
+
+        if ($remember) {
+            // Save session id in cookies
+            $sid = session_id();
+
+            $expire = time() + $days * 24 * 3600;
+            $domain = ""; // default domain
+            $secure = false;
+            $path = "/";
+
+            $cookie = setcookie("sid", $sid, $expire, $path, $domain, $secure, $http_only);
+        }
+    }
+
+    public function create($login, $password, $email, $name) { //добавил email и name
+        $user_exists = $this->getSalt($login);
+
+        if ($user_exists) {
+            throw new \Exception("User exists: " . $login, 1);
+        }
+
+        $query = "insert into users (login, password, email, name, salt)
+            values (:login, :password, :email, :name, :salt)";
+        $hashes = $this->passwordHash($password);
+        $sth = $this->db->prepare($query);
+
+        try {
+            $this->db->beginTransaction();
+            $result = $sth->execute(
+                array(
+                    ':login' => $login,
+                    ':password' => $hashes['hash'],
+                    ':email' => $email,
+                    ':name' => $name,
+                    ':salt' => $hashes['salt'],
+                )
+            );
+            $this->db->commit();
+        } catch (\PDOException $e) {
+            $this->db->rollback();
+            echo "Database error: " . $e->getMessage();
+            die();
+        }
+
+        if (!$result) {
+            $info = $sth->errorInfo();
+            printf("Database error %d %s", $info[1], $info[2]);
+            die();
+        } 
+
+        return $result;
+    }
+
+    public function connectdb($db_name, $db_user, $db_pass, $db_host = "localhost")
+    {
+        try {
+            $this->db = new \pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+        } catch (\pdoexception $e) {
+            echo "database error: " . $e->getmessage();
+            die();
+        }
+        $this->db->query('set names utf8');
+
+        return $this;
+    }
+}
