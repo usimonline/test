9ea93d323ec0a1246a399f465c11054c4dce3aa7
usimonline <usim.online@mail.ru>
2018-09-16 12:32:45 +0300

    1

--- start.php
+++ start.php
@@ -3,7 +3,7 @@
 if (!empty($_COOKIE['sid'])) {
 // check session id in cookies
 session_id($_COOKIE['sid']);
-    $_SESSION['name'] = $_COOKIE['sid'];
+    $_SESSION['name'] = $_COOKIE['name'];
 }
 
 session_start();
