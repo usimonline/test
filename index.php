0f35e8bc123c22daf3e8107e2ea89a8a693df405
usimonline <usim.online@mail.ru>
2018-09-17 21:11:44 +0300

    1

--- index.php
+++ index.php
@@ -19,17 +19,14 @@
              $db_user = "u689193950_user";
              $db_pass = "111111";
             try {
-                $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+                $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
             } catch (\pdoexception $e) {
                 echo "database error: " . $e->getmessage();
                 die();
             }
             $db->prepare("LOAD XML LOCAL INFILE 'users.xml' REPLACE INTO TABLE users ROWS IDENTIFIED BY '<database>'");
-            //$db->prepare('set names utf8');
             $db->execute();
             $db = null;
-
-
             ?>
 
             <?php if (Auth\User::isAuthorized()): ?>
