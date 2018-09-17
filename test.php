5b4ef22222645d4f37ec1509b3bc1cc5e8544f53
usimonline <usim.online@mail.ru>
2018-09-17 21:45:03 +0300

    1

--- test.php
+++ test.php
@@ -5,17 +5,17 @@ echo '1';
              $db_name = "u689193950_base";
              $db_user = "u689193950_user";
              $db_pass = "111111";
-            //$dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
-            try {
-               $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
-            } catch (\pdoexception $e) {
-               echo "database error: " . $e->getmessage();
-                die();
-           }
+             $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
+            //try {
+           //    $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
+           // } catch (\pdoexception $e) {
+           //    echo "database error: " . $e->getmessage();
+           //     die();
+          // }
             //$db->prepare('set names utf8');
             //$db->prepare("LOAD XML LOCAL INFILE 'users.xml' REPLACE INTO TABLE users ROWS IDENTIFIED BY '<database>'");
             $db->prepare('SELECT * from users');
             $db->execute();
-            $a = $db->rowCount();
-            echo $a;
+            //$a = $db->rowCount();
+            echo '2';
             $db = null;
