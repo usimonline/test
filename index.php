13fe5aa17c58e8fc57e80bf22a1d9682f951af08
usimonline <usim.online@mail.ru>
2018-09-17 21:14:46 +0300

    1

--- index.php
+++ index.php
@@ -18,13 +18,14 @@
              $db_name = "u689193950_base";
              $db_user = "u689193950_user";
              $db_pass = "111111";
-            try {
-                $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
-            } catch (\pdoexception $e) {
-                echo "database error: " . $e->getmessage();
-                die();
-            }
-            $db->prepare('set names utf8');
+            $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
+           // try {
+          //      $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
+           // } catch (\pdoexception $e) {
+           //     echo "database error: " . $e->getmessage();
+           //     die();
+          //  }
+          //  $db->prepare('set names utf8');
             //$db->prepare("LOAD XML LOCAL INFILE 'users.xml' REPLACE INTO TABLE users ROWS IDENTIFIED BY '<database>'");
            // $db->execute();
             $db = null;
