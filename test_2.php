b1df39f771afd6f77effc85bee6990b065e0b2a1
usimonline <usim.online@mail.ru>
2018-09-18 09:04:40 +0300

    1

--- test_2.php
+++ test_2.php
@@ -14,8 +14,8 @@ $stmt = $user->trtrtr();
 echo 1;
 $i = 0;
 foreach($stmt as $rows) {
-    echo $rows[0];
-    echo $rows[1];
+    echo $rows[0].'<br>';
+    echo $rows[1].'<br>';
     //echo $rows[2];
     //echo $rows[3];
     //echo $rows[4];
