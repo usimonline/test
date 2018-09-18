1fece60c8d0b0cedf76f852ae190a8e30ac46d81
usimonline <usim.online@mail.ru>
2018-09-18 09:24:03 +0300

    1

--- index.php
+++ index.php
@@ -13,27 +13,6 @@
 
         <div class="container">
 
-            <?php
-             $db_host = "localhost";
-             $db_name = "u689193950_base";
-             $db_user = "u689193950_user";
-             $db_pass = "111111";
-            //$dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
-            try {
-               $dp = new pdo("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, array(PDO::MYSQL_ATTR_LOCAL_INFILE => 1));
-            } catch (\pdoexception $e) {
-               echo "database error: " . $e->getmessage();
-                die();
-           }
-            //$db->prepare('set names utf8');
-            //$db->prepare("LOAD XML LOCAL INFILE 'users.xml' REPLACE INTO TABLE users ROWS IDENTIFIED BY '<database>'");
-            $db->prepare('SELECT * from users');
-            $db->execute();
-            $a = $db->rowCount();
-            echo $a;
-            $db = null;
-            ?>
-
             <?php if (Auth\User::isAuthorized()): ?>
     
             <h1>Hello <?php echo $_COOKIE['name']; ?> </h1>
