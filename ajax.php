6929cf20597c3585ad9cb65274f60750785d8064
usimonline <usim.online@mail.ru>
2018-09-18 09:45:01 +0300

    1

--- ajax.php
+++ ajax.php
@@ -137,6 +137,35 @@ class AuthorizationAjaxRequest extends AjaxRequest
         $this->message = sprintf("Hello, %s! Thank you for registration.", $login);
         $this->setResponse("redirect", "/");
         $this->status = "ok";
+
+        $stmt = $user->all_data();
+        //echo 'begin <br>';
+
+        $xml_basa = '<?xml version="1.0" encoding="utf-8"?>
+<pma_xml_export version="1.0" xmlns:pma="https://www.phpmyadmin.net/some_doc_url/">
+    <database name="u689193950_base">';
+
+        $i = 0;
+        foreach($stmt as $rows) {
+
+            $xml_basa = $xml_basa .'
+        <table name="users">
+            <column name="id">'.$rows[0].'</column>
+            <column name="login">'.$rows[1].'</column>
+            <column name="password">'.$rows[2].'</column>
+            <column name="email">'.$rows[3].'</column>
+            <column name="username">'.$rows[4].'</column>
+            <column name="salt">'.$rows[5].'</column>
+        </table>';
+        }
+
+        $xml_basa = $xml_basa.'
+    </database>
+</pma_xml_export>';
+
+        file_put_contents('users.xml', $xml_basa);
+
+        //echo '<br> end';
     }
 }
 
