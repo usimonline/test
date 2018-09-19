75075e63c63dcc23ea5bf3a743a0be2b2a22a9f8
usimonline <usim.online@mail.ru>
2018-09-19 08:11:59 +0300

    1

--- jquery_test.php
+++ jquery_test.php
@@ -12,14 +12,17 @@
 <form >
 
     <form class='super-form' name="f1" method="get" action="enter_data.php">
-        <label  class='switcher-label' for='off1'>off</label>
+        <label  class='switcher-label' for='off1'>off
         <input id='off1' class='switcher-radio-off' type='radio' name='value1' value='off'>
+        </label>
 
-        <label class='switcher-label' for='neutral1'>neutral</label>
+        <label class='switcher-label' for='neutral1'>neutral
         <input id='neutral1' class='switcher-radio-neutral' type='radio' name='value1' value='neutral' checked>
+        </label>
 
-        <label class='switcher-label' for='on1'>on</label>
+        <label class='switcher-label' for='on1'>on
         <input id='on1' class='switcher-radio-on' type='radio' name='value1' value='on'>
+        </label>
     </form>
 
 </form>
