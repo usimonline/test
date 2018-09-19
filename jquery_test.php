db4c88e6c9c29ff5b6a05cae7a36c33937efb113
usimonline <usim.online@mail.ru>
2018-09-19 07:45:29 +0300

    1

--- jquery_test.php
+++ jquery_test.php
@@ -11,21 +11,19 @@
 
 <form >
 
-    <form name="f1" method="get" action="enter_data.php">
-        <p><b>Lorem ipsum dolor sit amet...</b></p>
-        <p><input type="checkbox" id="check1"><label for="check1">Lorem</label><Br>
-            <input type="checkbox" id="check2"><label for="check2">Ipsum</label><Br>
-            <input type="checkbox" id="check3"><label for="check3">Dolor</label><Br>
-            <input type="checkbox" id="check4"><label for="check4">Sit amet</label></p>
-
-        <input name="link" type="hidden" value="index.php" />
-        Логин: <br />
-        <input name="login" type="text" size="25" maxlength="30" value="Вася" /> <br />
-        Пароль: <br />
-        <input name="pd" type="password" size="25" maxlength="30" value="" /> <br />
-        <input name="remember" type="checkbox" value="yes" /> Запомнить <br />
-
-        <input type="submit" name="enter" value="Вход" />
+    <form class='super-form' name="f1" method="get" action="enter_data.php">
+        <label>
+            <input type="checkbox" id="check1">Lorem<Br>
+            <input type="checkbox" id="check2">Ipsum<Br>
+            <input type="checkbox" id="check3">Dolor<Br>
+            <input type="checkbox" id="check4">Sit amet<br>
+         </label>
+        <label>
+            <input type="checkbox" id="check1">Lorem2<Br>
+            <input type="checkbox" id="check2">Ipsum2<Br>
+            <input type="checkbox" id="check3">Dolor2<Br>
+            <input type="checkbox" id="check4">Sit amet2<br>
+        </label>
     </form>
 
 </form>
